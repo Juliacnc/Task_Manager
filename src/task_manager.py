cc5e5792b2dd768dec33724c6b061b83f6790e14
Juliacnc <julia.canac@gmail.com>
2025-06-30 15:19:09 +0200

    ajout de la fonctionnalité : modifier une tache

--- src/task_manager.py
+++ src/task_manager.py
@@ -6,20 +6,31 @@ task_list = [
         "id": 1,
         "title": "Première tâche",
         "description": "Description de la première tâche",
-        "status": "TODO"
+        "status": "TODO",
     },
     {
         "id": 2,
         "title": "Deuxième tâche",
         "description": "Description de la deuxième tâche",
-        "status": "DONE"
-    }
+        "status": "DONE",
+    },
 ]
 
 
 def get_tasks() -> List[Dict]:
-    """
-    Récupère la liste des tâches
-    """
-    
+    """Récupère la liste des tâches"""
     return task_list
+
+
+def modify_task(
+    task_id: int, title: str, description: str, status: str
+) -> Dict:
+    """Modifie une tâche existante"""
+    for task in task_list:
+        if task["id"] == task_id:
+            task["title"] = title
+            task["description"] = description
+            task["status"] = status
+            return task
+
+    raise ValueError(f"Tâche avec l'ID {task_id} non trouvée.")
