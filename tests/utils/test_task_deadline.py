92c0c3d828dea9bede9b8725ca0fb10cc44dedb2
Juliacnc <julia.canac@gmail.com>
2025-07-03 10:40:46 +0200

    Correction des erreurs de tests

--- tests/utils/test_task_deadline.py
+++ tests/utils/test_task_deadline.py
@@ -6,10 +6,9 @@ from src.classes.errors import TaskNotFoundError
 class TestDeadlineTask:
     """Test class for DeadlineTask."""
 
-    @pytest.fixture
-    def sample_task_list():
+    def setup_method(self):
         """Fixture to provide a sample task list."""
-        return [
+        self.task = [
             {
                 "id": 1,
                 "title": "Task 1",
@@ -21,60 +20,56 @@ class TestDeadlineTask:
                 "title": "Task 2",
                 "description": "Description 2",
                 "created_at": "2024-01-02T10:00:00",
+                "deadline": "2024-12-31",
             },
             {
-                "id": 2,
-                "title": "Task 2",
-                "description": "Description 2",
-                "created_at": "2024-01-02T10:00:00",
+                "id": 3,
+                "title": "Task 3",
+                "description": "Description 3",
+                "created_at": "2024-01-03T10:00:00",
                 "deadline": "2024-12-31",
             },
         ]
 
-    def test_add_deadline_to_task(self, sample_task_list):
+    def test_add_deadline_to_task(self):
         """Test adding a deadline to a task."""
         task_id = 1
         deadline = "2024-12-31"
         deadline_task = DeadlineTask(
-            task_list=sample_task_list, task_id=task_id, deadline=deadline
+            task_list=self.task, task_id=task_id, deadline=deadline
         )
 
         deadline_task.add_deadline_to_task()
 
-        assert sample_task_list[0]["deadline"] == deadline
+        assert deadline_task.task["deadline"] == deadline
 
-    def test_modify_task_deadline(self, sample_task_list):
+    def test_modify_task_deadline(self):
         """Test modifying the deadline of a task."""
-        task_id = 1
+        task_id = 2
         new_deadline = "2025-01-01"
         deadline_task = DeadlineTask(
-            task_list=sample_task_list, task_id=task_id, deadline=new_deadline
+            task_list=self.task, task_id=task_id, deadline=new_deadline
         )
 
         deadline_task.modify_task_deadline()
-        assert sample_task_list[0]["deadline"] == new_deadline
+        assert deadline_task.task["deadline"] == new_deadline
 
-    def test_remove_deadline_from_task(self, sample_task_list):
+    def test_remove_deadline_from_task(self):
         """Test removing the deadline from a task."""
         task_id = 3
-        deadline = "2024-12-31"
-        deadline_task = DeadlineTask(
-            task_list=sample_task_list, task_id=task_id, deadline=deadline
-        )
+        deadline_task = DeadlineTask(task_list=self.task, task_id=task_id)
 
         deadline_task.remove_deadline_from_task()
-        assert "deadline" not in sample_task_list[0]
+        assert deadline_task.task["deadline"] == None
 
-    def test_invalid_task_id(self, sample_task_list):
+    def test_invalid_task_id(self):
         """Test handling of an invalid task ID."""
         with pytest.raises(
             TaskNotFoundError, match="Task not found with ID 999."
         ):
-            DeadlineTask(
-                task_list=sample_task_list, task_id=999
-            )  # Non-existent ID
+            DeadlineTask(task_list=self.task, task_id=999)  # Non-existent ID
 
-    def test_invalid_deadline_format(self, sample_task_list):
+    def test_invalid_deadline_format(self):
         """Test handling of an invalid deadline format."""
         task_id = 1
         invalid_deadline = "31-12-2024"
@@ -82,20 +77,18 @@ class TestDeadlineTask:
             ValueError, match="Deadline must be in 'YYYY-MM-DD' format."
         ):
             DeadlineTask(
-                task_list=sample_task_list,
+                task_list=self.task,
                 task_id=task_id,
                 deadline=invalid_deadline,
             )
 
-    def test_past_deadline(self, sample_task_list):
+    def test_past_deadline(self):
         """Test handling of a past deadline."""
         task_id = 1
         past_deadline = "2020-01-01"
-        with pytest.raises(
-            ValueError, match="Deadline cannot be in the past."
-        ):
+        with pytest.warns(UserWarning, match="The deadline is in the past"):
             DeadlineTask(
-                task_list=sample_task_list,
+                task_list=self.task,
                 task_id=task_id,
                 deadline=past_deadline,
             )
